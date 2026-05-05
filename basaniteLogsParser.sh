0346fa18232ee42ee1c0f6811d8863661334b667
Valer <36976806+BrightTunnel@users.noreply.github.com>
2026-05-05 09:18:52 -0400

    Update basaniteLogsParser.sh

--- basaniteLogsParser.sh
+++ basaniteLogsParser.sh
@@ -1,64 +1,48 @@
 #!/bin/bash
-#--Jira Node Failure Root Cause Analyzer. Scans jira + catalina logs for fatal patterns
-#--Seismo by Valeri Tikhonov, TD, April-May 2026.
+#--Atlassian Application Failure Root Cause Analyzer. Scans local application node logs for fatal error patterns.
+#--SeismoLog by Valeri Tikhonov, TD, May 2026.
 
-#sudo su - user
-#rm ${SeismoRCAReportLog}
-
-##--Logs:
-APP_INST="/opt/atlassian/jira/install/logs/"
-APP_INST="/opt/atlassian/confluence/install/logs/"
-APP_INST="${1:-/media/user/Storage/@jira_logs_copy/jira_sys/logs/}"
-APP_HOME="/opt/atlassian/jira/home/logs/"
-APP_HOME="/opt/atlassian/confluence/home/logs/" #PAT
-APP_HOME="${2:-/media/user/Storage/@jira_logs_copy/jira_home/log/}"
-JIRA_AUD="${APP_HOME}audit/"
-JIRA_JFR="${APP_HOME}jfr/"
-CATALINA_LOG=${2:-/home/user/atlassian-jira-software/logs/catalina.out}
-CATALINA_LOG="${APP_INST}catalina." #PAT e.g.: catalina.2026-03-14.log
-APP_MAIN_LOG=${1:-/home/user/atlassian-jira-home/log/atlassian-jira.log}
-APP_MAIN_LOG="${APP_HOME}atlassian-confluence.log" #PAT
-APP_MAIN_LOG="${APP_HOME}atlassian-jira-perf.log"
-APP_MAIN_LOG="${APP_HOME}atlassian-jira.log"
-JIRA_SLOW_JQL="${APP_HOME}atlassian-jira-slow-queries.log"
-
-catalinaLogName="${CATALINA_LOG##*/}"
-appLogName="${APP_MAIN_LOG##*/}"
-jiraSlowJql="${JIRA_SLOW_JQL##*/}"
 REPORTS="/media/user/Storage/lenovo-storage/@Mobile/@Wiki/Projects/@Java/@Jira/temp_collection_of_scripts_to_be_sorted/bash/tmp/seismo_FailureRCAnalysis_$(date +%Y%m%d-%H%M%S)/"
 mkdir -p "${REPORTS}"
-SeismoRCAReportLog="${REPORTS}seismoRCAReport.log"
-SeismoLogExcerpt="${REPORTS}seismoLogExcerpt.log"
-
-#--Notes:
-#--	Dont quote: ${LogNames} when used:
-#--	${LogsParsedInfo} for report header only, no business logic attached
-
-#--Events to watch:
-# Detect request timeout / slow endpoints
-# OutOfMemoryError, JVM memory (Heap space) failures
-
-#--Extra:
-#declare -A keywordsMapPiped #keywordsMapPiped[""]="" #--Linear Array style
-#keywordsMap["NodeShutdownTrigger"]="Shutdown|Stopping[[:space:]]Jira|Jira[[:space:]]is[[:space:]]shutting[[:space:]]down"
-#keywordsMap["Slow JQL"]="took[[:space:]][0-9]+[[:space:]]ms|Slow[[:space:]]request"
-#FilterOR+="|${keywordsMap["NodeShutdownTrigger"]}"
-#FilterOR+="|[[:space:]](WARNING|WARN)[[:space:]]"
+SeismoRCAReportLog="${REPORTS}sseismo_failure-analysis.log"
+SeismoLogExcerpt="${REPORTS}seismo_log_excerpt.log"
 
 
 #bash << 'EOF'
-#APP_INST="/opt/atlassian/confluence/install/logs/"
-#APP_HOME="/opt/atlassian/confluence/home/logs/"
-#CATALINA_LOG="${APP_INST}catalina."
-#APP_MAIN_LOG="${APP_HOME}atlassian-confluence.log"
-#SeismoRCAReportLog="seismoRCAReport.log"
-#SeismoLogExcerpt="seismoLogExcerpt.log"
-
-RangeStartDate="2026-02-10"; RangeStartTime="00:00"
+#SeismoRCAReportLog="sseismo_failure-analysis.log"
+#SeismoLogExcerpt="seismo_log_excerpt.log"
+RangeTopDate="2026-02-10"; RangeTopTime="00:00"
 RangeEndDate="2026-02-18"; RangeEndTime="23:59"
-ExcerptStartDate="2026-03-13"; ExcerptStartTime="10:37"
-ExcerptEndDate="2026-03-13"; ExcerptEndTime="10:38"
+XcrptTopDate="2026-03-13"; XcrptTopTime="10:37"
+XcrptEndDate="2026-03-13"; XcrptEndTime="10:38"
+SensitivitySys=3
+SensitivityHome=10
+choice="Conf"
+choice="Dbg"
+if [[ "$choice" == "Jira" ]]; then
+    APP_INST="/opt/atlassian/jira/install/logs/"
+	APP_HOME="/opt/atlassian/jira/home/logs/"
+	CATALINA_LOG="${APP_INST}catalina." #catalina.2026-03-14.log
+	APP_MAIN_LOG="${APP_HOME}atlassian-jira-perf.log"
+	APP_MAIN_LOG="${APP_HOME}atlassian-jira.log"
+elif [[ "$choice" == "Conf" ]]; then
+    APP_INST="/opt/atlassian/confluence/install/logs/"
+    APP_HOME="/opt/atlassian/confluence/home/logs/"
+    CATALINA_LOG="${APP_INST}catalina."
+    APP_MAIN_LOG="${APP_HOME}atlassian-confluence.log"
+elif [[ "$choice" == "Bitbucket" ]]; then
+    exit
+elif [[ "$choice" == "Dbg" ]]; then    
+	APP_INST="${1:-/media/user/Storage/@jira_logs_copy/jira_sys/logs/}"
+	APP_HOME="${2:-/media/user/Storage/@jira_logs_copy/jira_home/log/}"
+	CATALINA_LOG=${2:-/home/user/atlassian-jira-software/logs/catalina.out}
+	CATALINA_LOG="${APP_INST}catalina."
+	APP_MAIN_LOG=${1:-/home/user/atlassian-jira-home/log/atlassian-jira.log}
+	APP_MAIN_LOG="${APP_HOME}atlassian-jira.log"
+fi
 ExcerptOriginFileName=${APP_MAIN_LOG}
+#catalinaLogName="${CATALINA_LOG##*/}"
+#appLogName="${APP_MAIN_LOG##*/}"
 
 declare -A keywordsMap
 keywordsMap["Atlassian-errors"]="[[:space:]](ERROR|FATAL|SEVERE|CRITICAL)[[:space:]]"
@@ -90,21 +74,19 @@ convert_string_date_to_iso() {
 	echo "$formatted_date"
 }
 print_seismograph() {
-	#--Use the first argument as the scale, default to 10 if empty
-	local scale=${1:-10} 
-	#--Shift arguments so "$@" only contains the filenames
-	shift 
-	awk -v s="$scale" '{ printf "%s %s   %s ", $2, $3, $1; for(i=0; i<$1/s; i++) printf "*"; print "" }' "$@"
-	#awk '{printf "%s %s   %s ", $2, $3, $1; for(i=0; i<$1/10; i++) printf "*"; print ""}' "$@"
+	local scale=${1:-10}
+	local threshold=${2:-1}
+	shift; shift #shift arguments so "$@" contains only data
+	awk -v s="$scale" -v t="$threshold" '$1 > t { printf "%s %s  %s ", $2, $3, $1; for(i=0; i<$1/s; i++) printf "*"; print "" }' "$@"
 }
 date_range_full() {
-	awk -v start="$RangeStartDate $RangeStartTime" -v end="$RangeEndDate $RangeEndTime" '$1 " " $2 >= start && $1 " " $2 <= end'  "$@"
+	awk -v start="$RangeTopDate $RangeTopTime" -v end="$RangeEndDate $RangeEndTime" '$1 " " $2 >= start && $1 " " $2 <= end' "$@"
 }
 date_range_excerpt() {
-	awk -v start="$ExcerptStartDate $ExcerptStartTime" -v end="$ExcerptEndDate $ExcerptEndTime" '$1 " " $2 >= start && $1 " " $2 <= end'  "$@"
+	awk -v start="$XcrptTopDate $XcrptTopTime" -v end="$XcrptEndDate $XcrptEndTime" '$1 " " $2 >= start && $1 " " $2 <= end' "$@"
 }
 #--SYS TOMCAT INSTALL_LOG
-RollingDay=${RangeStartDate}
+RollingDay=${RangeTopDate}
 LogNames="" 
 LogsParsedInfo=""
 EndComparison=$(date -d "${RangeEndDate} + 1 day" +%Y-%m-%d)
@@ -120,19 +102,19 @@ while [ "${RollingDay}" != "$EndComparison" ]; do
 	RollingDay=$(date -d "${RollingDay} + 1 day" +%Y-%m-%d)
 done
 echo "~Parsing SYS logs..." #Debug/Verbose
-echo -e "~INSTALL logs in range from ${RangeStartDate} ${RangeStartTime} to ${RangeEndDate} ${RangeEndTime}:${LogsParsedInfo}" > ${SeismoRCAReportLog}
+echo -e "~INSTALL logs in range from ${RangeTopDate} ${RangeTopTime} to ${RangeEndDate} ${RangeEndTime}:${LogsParsedInfo}" > ${SeismoRCAReportLog}
 if [[ -n "$LogNames" ]]; then
 	echo "~Events: ${FilterOR}" >> "${SeismoRCAReportLog}"
 	echo "~SeismoGraph:" >> "${SeismoRCAReportLog}"
 	LeadingDateRegex="^[0-9]{2}-[A-Z][a-z]{2}-[0-9]{4}"
-	#grep -Eh ${LeadingDateRegex} ${LogNames} | awk '$1 " " $2 >= "01-Jan-2026 00:00" && $1 " " $2 <= "30-Apr-2026 00:00"' | grep -E ${FilterOR} | sort | cut -c 1-17 | uniq -c | awk '{printf "%s %s   %s ", $2, $3, $1; for(i=0; i<$1; i++) printf "*"; print ""}' >> ${SeismoRCAReportLog}
+	#grep -Eh ${LeadingDateRegex} ${LogNames} | awk '$1 " " $2 >= "01-Jan-2026 00:00" && $1 " " $2 <= "30-Apr-2026 00:00"' | grep -E ${FilterOR} | sort | cut -c 1-17 | uniq -c | awk '{printf "%s %s %s ", $2, $3, $1; for(i=0; i<$1; i++) printf "*"; print ""}' >> ${SeismoRCAReportLog}
 	#test: grep -Eh ${LeadingDateRegex} ${LogNames} |grep -E ${FilterOR} | sort | cut -c 1-16 | uniq -c | print_seismograph 1 >> ${SeismoRCAReportLog} >> ${SeismoRCAReportLog}
 	#grep -Eh ${LeadingDateRegex} ${LogNames} >> ${SeismoRCAReportLog}
 
 	grep -Eh "${LeadingDateRegex}" ${LogNames} | while read -r date_str time_str rest; do
-   		iso_date=$(convert_string_date_to_iso "${date_str}")
+		iso_date=$(convert_string_date_to_iso "${date_str}")
 		echo "${iso_date} ${time_str} ${rest}"
-		done | date_range_full | grep -E ${FilterOR} | sort | cut -c 1-16 | uniq -c | print_seismograph 1 >> ${SeismoRCAReportLog} 
+		done | date_range_full | grep -E ${FilterOR} | sort | cut -c 1-16 | uniq -c | print_seismograph 1 "$SensitivitySys" >> ${SeismoRCAReportLog} 
 else
 	echo "~SeismoGraph~ No INSTALL_LOG data found." >> ${SeismoRCAReportLog}
 fi
@@ -143,7 +125,7 @@ LogNames="${LogNamesArr[0]}"
 for ((i=1; i<11; i++)); do
 	if [[ -f "${APP_MAIN_LOG}.${i}" ]]; then
 		LogNamesArr+=("${APP_MAIN_LOG}.${i}")
-		 #--Conat log files. Separate file names by space
+		#--Conat log files. Separate file names by space
 		LogNames+=" ${LogNamesArr[i]}"
 		echo "~Add to scope ${LogNamesArr[i]}" #Debug/Verbose
 	#else #echo "Warning: file ${APP_MAIN_LOG}.${i} not found" >&2
@@ -152,17 +134,17 @@ done
 echo "~Parsing HOME logs..." #Debug/Verbose
 if [[ "${#LogNamesArr[@]}" -gt 1 ]]; then
 	echo >> ${SeismoRCAReportLog}
-	echo -e "~HOME logs in range from ${ExcerptStartDate} ${RangeStartTime} to ${RangeEndDate} ${RangeEndTime}:\n${LogNames// /\\n}" >> ${SeismoRCAReportLog}
+	echo -e "~HOME logs in range from ${RangeTopDate} ${RangeTopTime} to ${RangeEndDate} ${RangeEndTime}:\n${LogNames// /\\n}" >> ${SeismoRCAReportLog}
 	echo "~Events: ${FilterOR}" >> ${SeismoRCAReportLog}
 	echo "~SeismoGraph:" >> ${SeismoRCAReportLog}
-	grep -Eh ${LeadingIsoDateRegex} ${LogNames} | date_range_full | grep -E ${FilterOR} | sort | cut -c 1-16 | uniq -c | print_seismograph 20 >> ${SeismoRCAReportLog}
+	grep -Eh ${LeadingIsoDateRegex} ${LogNames} | date_range_full | grep -E ${FilterOR} | sort | cut -c 1-16 | uniq -c | print_seismograph 20 "$SensitivityHome" >> ${SeismoRCAReportLog}
 else
 	echo "~SeismoGraph~ No HOME_LOG found." >> ${SeismoRCAReportLog}
 fi
 
 #-HOME_LOG_NARROW_EXCERPT
 echo "~Extracting Excerpt from the: ${ExcerptOriginFileName}..." #Debug/Verbose
-echo -e "~HOME logs NARROW_EXCERPT in range from ${ExcerptStartDate} ${ExcerptStartTime} to ${ExcerptEndDate} ${ExcerptEndTime}:\n${ExcerptOriginFileName// /\\n}" > ${SeismoLogExcerpt}
+echo -e "~HOME logs NARROW_EXCERPT in range from ${XcrptTopDate} ${XcrptTopTime} to ${XcrptEndDate} ${XcrptEndTime}:\n${ExcerptOriginFileName// /\\n}" > ${SeismoLogExcerpt}
 echo "" >> ${SeismoLogExcerpt}
 grep -Eh ${LeadingIsoDateRegex} ${ExcerptOriginFileName} | date_range_excerpt | sed 'G' >> "${SeismoLogExcerpt}"
 
@@ -172,9 +154,9 @@ echo "~SeismoGraph: eof" >> ${SeismoRCAReportLog}
 #EOF
 #====================
 #====================
-
 exit 0
-#todo: HTTP/1.1" 200
+
+
 
 
 
