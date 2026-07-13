f57d1def4302c6451916809704cd3ad06616bf45
Valer <36976806+BrightTunnel@users.noreply.github.com>
2026-07-13 13:18:16 -0400

    Update basaniteLogsParser.sh

--- basaniteLogsParser.sh
+++
@@ -1,7 +0,0 @@
-
-
-Thanks, Diego. Yes, I spoke with Cathal and confirmed that those are only thread dump files. Thank you for pointing that out.
-
-I'm now uploading the Confluence home directory log files that were saved separately at that point in time. They are zipped by individual node. The files are attached below.
-
-Please let me know if you need any additional data from us.
